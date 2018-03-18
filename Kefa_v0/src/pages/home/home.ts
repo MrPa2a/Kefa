fcad7fa039e4175385e446e370028950b9e370f9
VincentCreusy <vincentcreusy@gmail.com>
2018-03-18 23:27:04 +0100

    Fusion avec le code de Vincent

--- Kefa_v0/src/pages/home/home.ts
+++ Kefa_v0/src/pages/home/home.ts
@@ -1,35 +1,57 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef, NgModule, Injectable } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { FirebaseProvider } from './../../providers/firebase/firebase';
-import { AngularFireList} from 'angularfire2/database';
+import { AngularFireList } from 'angularfire2/database';
+import { NativeGeocoder, NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
 
 declare var google;
 
+export interface PlaceData {
+  name: string,
+  address: string
+}
+export interface Coordinates {
+  lat: number,
+  lng: number
+}
+
 @Component({
   selector: 'home-page',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [
+    NativeGeocoder
+  ]
 })
 export class HomePage {
-
   @ViewChild('map') mapElement: ElementRef;
+  @ViewChild(Slides) slides: Slides;
+
   placesItems: any;
   newItem = '';
   map: any;
+  markers: any;
 
-  /*constructor(public navCtrl: NavController, public geolocation: Geolocation) {
-
-  }*/
-
-  constructor(public navCtrl: NavController, public firebaseProvider: FirebaseProvider, public geolocation: Geolocation) {
+  constructor(public firebaseProvider: FirebaseProvider, public navCtrl: NavController, public geolocation: Geolocation, public nativeGeocoder: NativeGeocoder) {
     this.placesItems = this.firebaseProvider.getPlacesItems();
-    console.log("La liste c'est" +this.placesItems);
   }
- 
+
+  getObjectWithoutKnowingKey(data) {
+    let objects = [];
+
+    for (var propName in data) {
+        if (data.hasOwnProperty(propName)) {
+            objects.push(data[propName]);
+        }
+    }
+
+    return objects;
+  }
+
   addItem() {
     this.firebaseProvider.addItem(this.newItem);
   }
- 
+
   removeItem(id) {
     this.firebaseProvider.removeItem(id);
   }
@@ -38,53 +60,77 @@ export class HomePage {
     this.loadMap();
   }
 
-  loadMap() {
-
-    this.geolocation.getCurrentPosition().then((position) => {
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
+  addMarkerWithPlace(place: PlaceData) {
+    this.getCoordinatesFromAdress(place.address).then((coord) => {
+      this.addMarker(coord);
     }, (err) => {
-      let latLng = new google.maps.LatLng(43.697233, 7.27023650000001);
+      console.log('Error at HomePage.addMarkerWithPlace() : ' + err)
+    });
+  }
 
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+  slideChanged() {
+    let currentIndex = this.slides.getActiveIndex();
+    let array = this.getObjectWithoutKnowingKey(this.placesItems);
+    this.addMarkerWithPlace(array[currentIndex]);
+  }
+
+  loadMap() : void {
+    this.geolocation.getCurrentPosition().then((position) => {
+      this.createMap(position.coords.latitude, position.coords.longitude);
+    }, (err) => {
+      this.createMap(43.697233, 7.27023650000001);
+    })
+  }
+
+  createMap(lat: number, lng: number) : void {
+    let latLng = new google.maps.LatLng(lat, lng);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+
+  getCoordinatesFromAdress(address: string) : Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.nativeGeocoder.forwardGeocode(address)
+      .then((coordinates: NativeGeocoderForwardResult) => {
+        resolve({
+          lat: coordinates[0].latitude,
+          lng: coordinates[0].longitude
+        })
+      })
+      .catch((error: any) => {
+        reject(error)
+      });
+    });
+  }
+
+  addMarker(coord: Coordinates) : void {
+    if (coord === undefined) {
+      let val1 = Math.floor(Math.random()*90);
+      let val2 = Math.floor(Math.random()*90);
+
+      coord = {
+        lat: val1,
+        lng: val2
       }
+    }
 
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-    });
+    var myLatLng = new google.maps.LatLng(coord.lat, coord.lng)
 
+    if (this.markers === undefined) {
+      this.markers = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: myLatLng
+      });
+    }
+    else {
+      this.markers.setPosition(myLatLng);
+    }
   }
-
-  /*addMarker() {
-    let marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: this.map.getCenter()
-    });
-
-    let content = "Informations";
-
-    this.addInfoWindow(marker, content);
-  }
-
-  addInfoWindow(marker, content) {
-    let infoWindow = new google.maps.InfoWindow({
-      content: content
-    });
-
-    google.maps.event.addListener(marker, 'click', () => {
-      infoWindow.open(this.map, marker);
-    });
-
-  }*/
 }
--- Kefa_v0/src/providers/firebase/firebase.ts
+++ Kefa_v0/src/providers/firebase/firebase.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase} from 'angularfire2/database';
- 
+import { PlaceData } from '../../pages/home/home';
+
 @Injectable()
 export class FirebaseProvider {
- 
+
   constructor(public afd: AngularFireDatabase) { }
- 
+
   getPlacesItems() {
-    return this.afd.list('/placesItems/').valueChanges();
+    return this.afd.list<PlaceData>('/placesItems/').valueChanges();
   }
- 
+
   addItem(name) {
     this.afd.list('/placesItems/').push(name);
   }
- 
+
   removeItem(id) {
     this.afd.list('/placesItems/').remove(id);
   }
